53f9836d9b152979721165b9e67c8b3a0dbb4e1f
MalickArsalanLFD <malick.arsalan@lovefordata.com>
2023-10-16 00:28:25 +0500

    Creating More Customize Widgets
    * Also Splitting Codes Across Files

--- 2. Getting a Solid Foundation/roll_dice/lib/gradient.container.dart
+++ 2. Getting a Solid Foundation/roll_dice/lib/gradient.container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -17,10 +18,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(color: Colors.white, fontSize: 28.0),
-        ),
+        child: StyledText(),
       ),
     );
   }
