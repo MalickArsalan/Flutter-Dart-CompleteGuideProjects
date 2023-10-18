f133a9f9c9d347e78121f73f7215422ce341400c
MalickArsalanLFD <malick.arsalan@lovefordata.com>
2023-10-18 15:53:21 +0500

    Converting Custom Gradient Widget
        * Into Usable Custom Gradient Widget
        * Making correction in the file name of Custom Gradient.

--- 2. Getting a Solid Foundation/roll_dice/lib/main.dart
+++ 2. Getting a Solid Foundation/roll_dice/lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice/gradient.container.dart';
+import 'package:roll_dice/gradient_container.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: Colors.deepPurple, body: GradientContainer()),
+        backgroundColor: Colors.deepPurple,
+        body: GradientContainer(
+          Color.fromARGB(255, 33, 5, 109),
+          Color.fromARGB(255, 68, 21, 149),
+        ),
+      ),
     ),
   );
 }
