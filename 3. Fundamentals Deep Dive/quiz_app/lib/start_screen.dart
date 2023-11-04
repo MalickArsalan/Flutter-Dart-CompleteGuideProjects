a941329a2267e8fbbe48362e317db680f3e86ff4
MalickArsalanLFD <malick.arsalan@lovefordata.com>
2023-11-04 22:14:37 +0500

    Landing Page Design Complete
    * Name of Landing Page changed to StartScreen
    * Added Outline Button wit Icon
    * Added Transparent Image

---
+++ 3. Fundamentals Deep Dive/quiz_app/lib/start_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          /* Opacity(
+            opacity: 0.5,
+            child: Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+            ),
+          ), */
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 60,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 24,
+              color: Color.fromARGB(255, 237, 223, 252),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
+        ],
+      ),
+    );
+  }
+}
