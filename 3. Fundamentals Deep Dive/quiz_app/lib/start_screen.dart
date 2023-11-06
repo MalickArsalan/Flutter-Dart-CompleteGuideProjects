b0bbb1beb74a79a45368221f74f688efa7a9b5a9
MalickArsalanLFD <malick.arsalan@lovefordata.com>
2023-11-06 13:42:41 +0500

    Render Content Conditionally & Lifting State
    * We created a parent screen that helps us in:
      * Lifting State
      * Rendering content conditionally
    
    * We Created a function in parent Widget and passed that function as a parameter into StartScreen
    * The button inside the StartScreen trigger the parent method that change the state and render new content

--- 3. Fundamentals Deep Dive/quiz_app/lib/start_screen.dart
+++ 3. Fundamentals Deep Dive/quiz_app/lib/start_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -34,7 +37,7 @@ class StartScreen extends StatelessWidget {
             height: 30,
           ),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
             icon: const Icon(Icons.arrow_right_alt),
             label: const Text('Start Quiz'),
