0e210cded53acf3ea8376d47812f8a1c3f6f7df3
MalickArsalanLFD <malick.arsalan@lovefordata.com>
2023-11-10 15:50:55 +0500

    Passing Data via Functions Across Widgets

--- 3. Fundamentals Deep Dive/quiz_app/lib/quiz.dart
+++ 3. Fundamentals Deep Dive/quiz_app/lib/quiz.dart
@@ -10,6 +10,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  final List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   /* 
@@ -28,12 +29,16 @@ class _QuizState extends State<Quiz> {
     });
   }
 
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+  }
+
   @override
   Widget build(context) {
     Widget screenWidget = StartScreen(switchScreen);
 
     if (activeScreen == 'questions-screen') {
-      screenWidget = const QuestionsScreen();
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
     }
 
     return MaterialApp(
