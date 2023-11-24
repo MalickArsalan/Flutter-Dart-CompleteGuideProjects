f799b5712b32ab4418eb7f5289f898f62c25bc3d
MalickArsalan Home <malickarsalan@hotmail.com>
2023-11-24 07:37:06 +0500

    Created Dummy Expenses
    * Created a dummy data to display data in our widgets
    * Created CustomWidget for ExpenseList
    * Used this ExpenseList inside Expenses and wrapped this widget inside Expanded widget.

--- 5. Interactivity MoreWidgets Theming/expense_tracker/lib/expenses.dart
+++ 5. Interactivity MoreWidgets Theming/expense_tracker/lib/expenses.dart
@@ -1,4 +1,7 @@
+import 'package:expense_tracker/expenses_list.dart';
 import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -7,13 +10,26 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('The chart'),
-          Text('Expenses list...'),
+          const Text('The chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses))
         ],
       ),
     );
