08369d6136ed3c3642b1c2506a5c7a0f12ae79f6
MalickArsalan Home <malickarsalan@hotmail.com>
2023-11-29 09:14:13 +0500

    Added Dropdown Button to select Expense Type

--- 5. Interactivity MoreWidgets Theming/expense_tracker/lib/widgets/new_expense.dart
+++ 5. Interactivity MoreWidgets Theming/expense_tracker/lib/widgets/new_expense.dart
@@ -14,6 +14,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _amountController = TextEditingController();
 
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
@@ -79,9 +80,31 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ],
           ),
+          const SizedBox(
+            height: 16,
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          value: category,
+                          child: Text(category.name.toUpperCase()),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    if (value == null) {
+                      return;
+                    }
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  }),
+              Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
